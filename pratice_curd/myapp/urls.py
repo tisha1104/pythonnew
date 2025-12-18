5977a426c3dc09b85c1a919313dee762e39b9467
tisha <tisha@gmail.com>
2025-12-18 15:12:51 +0530

    all commit

---
+++ pratice_curd/myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from myapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+        path("",index,name="index"),
+        path("delete",delete_product,name="delete"),
+        path("edit",edit_product,name="edit")
+]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ pratice_curd/myapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect
+from myapp.models import *
+
+# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    if request.method =='POST':
+        name = request.POST['name']
+        price = request.POST['price']
+        qty = request.POST['qty']
+        image = request.FILES['image']
+        Product.objects.create(name=name,price=price,qty=qty,image=image)
+        return render(request,"index.html",{"success":"Product Insert Succesfully","products":products})
+    else:
+        return render(request,"index.html",{"products":products})
+    
+def delete_product(request):
+    id = request.GET['id']
+    p = Product.objects.get(pk=id)
+    p.delete()
+    return redirect("index")
+
+def edit_product(request):
+    products = Product.objects.all()
+    if request.method =='POST':
+        id= request.POST['id']
+        name= request.POST['name']
+        qty= request.POST['qty']
+
+        p= Product.objects.get(pk=id)
+        p.name=name
+        p.qty=qty
+        if request.FILES:
+            p.image = request.FILES['image']
+        p.save()
+        return render(request,"index.html",{"products":products})
+    else:
+        id= request.GET['id']
+        p= Product.objects.get(pk=id)
+        return render(request,"index.html",{"p":p,"products":products})
