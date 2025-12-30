a00c71b5c57b2bbf4df4614c4207c9670eed7587
tisha <tisha@gmail.com>
2025-12-30 11:16:00 +0530

    all commit

--- AJAX_curd1/myapp/urls.py
+++ AJAX_curd1/myapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from myapp.views import *
 
 urlpatterns=[
-    path("",index,name="index")
+    path("",index,name="index"),
+    path("register",register,name="register"),
+    path("display",display,name="display")
 ]
