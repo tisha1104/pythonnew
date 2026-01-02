40b68169f68dfc89805a7dfaa042a0609e286124
tisha <tisha@gmail.com>
2026-01-02 13:05:13 +0530

    all commit

--- AJAX_curd/myapp/views.py
+++ AJAX_curd/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from myapp.models import *
 from django.http import JsonResponse ,HttpResponse
+from django.db.models import Q
 # Create your views here.
 
 def index(request):
@@ -21,4 +22,35 @@ def display(request):
     studentes=student.objects.all()
     return JsonResponse({"studentes":list(studentes.values())})
 
-    
+def delete_student(request):
+    sid=request.GET['sid']
+    st=student.objects.get(pk=sid)
+    st.delete()
+    return HttpResponse("Student Record Deleted Succesfully!") 
+
+def getbyid(request):
+    sid=request.GET['sid']
+    st=student.objects.filter(id=sid)
+    return JsonResponse({"studentes":list(st.values())}) 
+
+def update_student(request):
+      if request.method=='POST':
+        data= request.POST
+        id=data.get('id')
+        name = data.get('name')
+        email = data.get('email')
+        phone = data.get('phone')
+
+        st=student.objects.get(pk=id)
+        st.name=name
+        st.email=email
+        st.phone=phone
+        st.save()
+        return HttpResponse("Student Reoerd Updaeted Successfully!")
+      
+
+def search(request):
+    value=request.GET['value']
+    studentes=student.objects.filter(Q(name__startswith=value)|Q(email__startswith=value)|Q(phone__startswith=value))
+
+    return JsonResponse({"studentes":list(studentes.values())})
