7ea85607350ac54a324cadefd63009df336de50a
swapnilPatil-4740 <swapnilpatil4740@gmail.com>
2026-02-14 12:39:10 +0530

    all commit

--- eshoprest/myapp/urls.py
+++ eshoprest/myapp/urls.py
@@ -1,6 +1,12 @@
-from django.urls import *
+from django.urls import path,include
 from myapp.views import *
+from rest_framework import routers
+
+router=routers.DefaultRouter()
+router.register("users",UserViewSet)
+router.register("categories",CategoryViewSet)
+router.register("products",ProductViewSet)
 
 urlpatterns=[
-    
+    path('',include(router.urls))
 ]
--- eshoprest/myapp/views.py
+++ eshoprest/myapp/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
-
+from myapp.models import *
+from myapp.seriaizer import *
+from rest_framework.permissions import AllowAny ,IsAdminUser,IsAuthenticated
+from rest_framework.decorators import api_view,APIView
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    def get_permissions(self):
+        if self.action=='list':
+            permission_classes=[AllowAny]
+        elif self.action=='create':
+            permission_classes=[AllowAny]
+        elif self.action=='retrieve':
+            permission_classes=[AllowAny]
+        elif self.action=='destroy':
+            permission_classes=[AllowAny]
+        elif self.action=='update':
+            permission_classes=[AllowAny]
+        else :
+            print(self.action)
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+    
+
+class CategoryViewSet(ModelViewSet):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+    def get_permissions(self):
+        if self.action=='list':
+            permission_classes=[AllowAny]
+        elif self.action=='create':
+            permission_classes=[AllowAny]
+        elif self.action=='retrieve':
+            permission_classes=[AllowAny]
+        elif self.action=='destroy':
+            permission_classes=[AllowAny]
+        elif self.action=='update':
+            permission_classes=[AllowAny]
+        else :
+            print(self.action)
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+    
+
+class  ProductViewSet(ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    def get_permissions(self):
+        if self.action=='list':
+            permission_classes=[AllowAny]
+        elif self.action=='create':
+            permission_classes=[AllowAny]
+        elif self.action=='retrieve':
+            permission_classes=[AllowAny]
+        elif self.action=='destroy':
+            permission_classes=[AllowAny]
+        elif self.action=='update':
+            permission_classes=[AllowAny]
+        else :
+            print(self.action)
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
