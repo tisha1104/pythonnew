00975d5f9f504c57e8a89c6e3280fc93822cd0a1
swapnilPatil-4740 <swapnilpatil4740@gmail.com>
2026-03-05 13:31:15 +0530

    all commit

---
+++ class/logical-2.py
@@ -0,0 +1,64 @@
+print("========palidrom==========")
+n=12345654321
+temp=n
+sum=0
+
+while n!=0:
+    rem=n%10
+    sum=sum*10+rem
+    n=n//10
+
+if (sum==temp):
+    print("number is palidrom")
+else:
+    print("number is not palidrom")
+
+print("=============Fibonacci==========")
+n=int(input("Enter the number"))
+a=0
+b=1
+for i in range(n):
+    print(a,end=" ")
+    c=a+b
+    a=b
+    b=c
+
+
+print("=============Fibonacci Using Recursion==========")
+
+
+def fibonacci(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+    
+num=input("enter the number")
+for i in range(n):
+    print(fibonacci(i),end=" ")
+
+print("=============Factorial==========")
+
+n=int(input("Enter the number"))
+fact=1
+
+while n!=0:
+    fact=n*fact
+    n-=1
+print(fact)
+
+print("==============Prime number=========")
+
+n=int(input("Enter the Number"))
+flag=0
+
+for i in range(2,n):
+    if n%i==0:
+        flag=1
+        break
+if flag==0:
+    print("number is prime")
+else:
+    print("numberis not prime")
---
+++ class/logical.py
@@ -0,0 +1,138 @@
+
+
+
+# number = 12345654321
+# temp = number
+# sum = 0
+# while number!=0:
+#     rem = number%10
+#     sum = sum*10 +rem
+#     number = number//10
+
+# if (sum==temp):
+#     print(f"{temp} is palidrom number")
+# else :
+#     print(f"{temp} is not palidrom number") 
+
+# *
+# **
+# ***
+# ****
+# *****
+
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+
+print("====================================================================================")
+
+# * * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()       
+
+print("====================================================================================")
+#              *
+#            * *
+#          * * *
+#        * * * *
+#      * * * * *
+for i in range(1,6):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+print("====================================================================================")
+
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+for i in range(1,6):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+print("====================================================================================")
+
+#  * * * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
+for i in range(6,0,-1):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+
+print("====================================================================================")
+
+
+# * * * * *
+# *       *
+# *       *
+# *       *
+# * * * * *
+for i in range(6):
+    for j in range(6):
+        if i==0 or j==0 or i==5 or j==5:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("====================================================================================")
+
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
+
+for i in range(6,0,-1):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+print("====================================================================================")
+
+
+#  * * * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+for i in range(6,0,-1):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+for i in range(2,7):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
