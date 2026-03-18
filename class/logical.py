6aa847f8272521f31b3d159d97ab3a09cec2b4db
swapnilPatil-4740 <swapnilpatil4740@gmail.com>
2026-03-18 12:12:09 +0530

    add

--- class/logical.py
+++ class/logical.py
@@ -136,3 +136,115 @@ for i in range(2,7):
     for j in range(2*i-1):
         print("*",end=" ")
     print()
+
+print("====================================================================================")
+
+#           *
+#         * * *
+#        * * * *
+#       * * * * *
+#     * * * * * * *
+#   * * * * * * * * *
+# * * * * * * * * * * *
+#   * * * * * * * * *
+#     * * * * * * *
+#       * * * * *
+#         * * *
+#           *
+for i in range(1,6):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+for i in range(6,0,-1):
+    for k in range(6-i):
+        print(" ",end=" ")
+    for j in range(2*i-1):
+        print("*",end=" ")
+    print()
+
+print("====================================================================================")
+
+# *                     *
+# * *                 * *
+# * * *             * * *
+# * * * *         * * * *
+# * * * * *     * * * * *
+# * * * * * * * * * * * *
+# * * * * * * * * * * * *
+# * * * * *     * * * * *
+# * * * *         * * * *
+# * * *             * * *
+# * *                 * *
+# *                     *
+for i in range(1,6+1):
+    for j in range(i):
+        print("*",end=" ")
+    for k in range(2*(6-i)):
+        print(" ",end=" ")
+    for a in range(i):
+        print("*",end=" ")
+    print()
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    for k in range(2*(6-i)):
+        print(" ",end=" ")
+    for a in range(i):
+        print("*",end=" ")
+    print()
+
+print("====================================================================================")
+
+# *
+# * *
+# *   *
+# *     *
+# *       *
+# * * * * * *
+for i in range(6):
+    for j in range(6):
+        if j==0 or j==i or i==5:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+print("====================================================================================")
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+print("====================================================================================")
+
+# * * * * * *
+# *       *
+# *     *
+# *   *
+# * *         
+# *
+for i in range(6):
+    for j in range(6):
+        if i==0 or j==0 or j==6-i-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
